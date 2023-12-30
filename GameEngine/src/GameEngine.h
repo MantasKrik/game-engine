ba3d3f4eebd0213d73cdef1d1b7e2c21498837a7
Mantas <47982279+MantasKrik@users.noreply.github.com>
2023-12-30 17:04:27 +0200

    Created entry point for the application

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Inteded for use by applications
+
+#include "GameEngine/Application.h"
+
+// --- Entry point ---------------
+#include "GameEngine/EntryPoint.h"
+// -------------------------------
---
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Application.h"
+
+#ifdef E_PLATFORM_WINDOWS
+
+extern GameEngine::Application* GameEngine::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	auto app = GameEngine::CreateApplication();
+
+	app->Run();
+
+	delete app;
+}
+
+#endif
